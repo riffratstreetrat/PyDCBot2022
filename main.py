50f5ff873cc4d0b78246674e5bb7c75fa94e92f5
riffratstreetrat <tecoffey14@gmail.com>
2022-11-02 22:50:59 -0400

    Final Theresa

--- main.py
+++ main.py
@@ -27,13 +27,36 @@ async def on_message(message):
         return
 
     if message.content.startswith('$hello'):
-        await message.channel.send('Do you have a dead uncle?')
+        await message.channel.send('Do you have a dead uncle? ($yes/$no)')
         y["counter"]+=1
-        await message.channel.send(y["counter"])
+        return
+
+
+    if message.content.startswith('$yes') and y["counter"] == 1:
+        await message.channel.send('I am sorry to hear that...*cough* *cough*...did he die by choking on a meatball parm? ($yes/$no)')
+        y["counter"]+=1
+        return
+
+    if message.content.startswith('$no') and y["counter"] == 1:
+        await message.channel.send('I am shocked! Perhaps I have lost my gift for communicating with the dead. Goodbye!')
+
+
+    elif message.content.startswith('$yes') and y["counter"] == 2:
+        await message.channel.send('Figures...That is how most of us die here on Long Island. Just know that your Uncle is at peace and very proud of you. Goodbye!')
+        y["counter"]+=1
+        return
+
+    if message.content.startswith('$no') and y["counter"] == 2:
+        await message.channel.send('Hmm perhaps it was *cough* ravioli?. ($yes)')
+        y["counter"]+=1
+        return
+
+    if message.content.startswith('$yes') and y["counter"] == 3:
+        await message.channel.send('AHA! You can never stump the Long Island Medium. Goodbye!')
         
 
-
-       
-
+    if message.content.startswith('Theresa tell me your life story'):
+        await message.channel.send('AHA! You can never stump the Long Island Medium. Goodbye!')
+        
 
 client.run(os.getenv('TOKEN'))
